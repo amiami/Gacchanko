a804ebfa908092ca88e5be13c68e217f68e6d0dc
amiami <ishiami@gmail.com>
2026-08-12 19:34:28 +0900

    feat: 初期4モデルのマイグレーション反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_12_085557) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "kit_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "item_name"
+    t.bigint "kit_id", null: false
+    t.datetime "updated_at", null: false
+    t.index ["kit_id"], name: "index_kit_items_on_kit_id"
+  end
+
+  create_table "kits", force: :cascade do |t|
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "select_type"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "list_items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.boolean "is_checked"
+    t.string "item_name"
+    t.bigint "packing_list_id", null: false
+    t.string "source_labels"
+    t.datetime "updated_at", null: false
+    t.index ["packing_list_id"], name: "index_list_items_on_packing_list_id"
+  end
+
+  create_table "packing_lists", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.date "event_date"
+    t.string "name"
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "kit_items", "kits"
+  add_foreign_key "list_items", "packing_lists"
 end
