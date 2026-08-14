e80daad2ac90215714439a7108779cfaadc0009a
amiami <ishiami@gmail.com>
2026-08-15 01:37:47 +0900

    feat: 持ち物準備の進捗確認機能

--- app/models/packing_list.rb
+++ app/models/packing_list.rb
@@ -1,4 +1,17 @@
+# app/models/packing_list.rb
 class PackingList < ApplicationRecord
   has_many :list_items, dependent: :destroy
   validates :name, presence: true
+
+  def checked_count
+    list_items.count(&:is_checked)
+  end
+
+  def total_count
+    list_items.size
+  end
+
+  def completed?
+    total_count > 0 && checked_count == total_count
+  end
 end
