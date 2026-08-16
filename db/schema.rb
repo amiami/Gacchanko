31bff73842c591a34b2e4b85a27fcf7e270ba936
amiami <ishiami@gmail.com>
2026-08-16 14:09:01 +0900

    feat: 認証機能のファイル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_12_085557) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_16_045619) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,24 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_12_085557) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "ip_address"
+    t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "kit_items", "kits"
   add_foreign_key "list_items", "packing_lists"
+  add_foreign_key "sessions", "users"
 end
